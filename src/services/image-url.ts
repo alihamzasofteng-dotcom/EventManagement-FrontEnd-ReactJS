d9c1522773267282c1965cab73c49dfbfced8bbd
Hamza Ali <hamza.ali@ethizo.com>
2024-10-22 17:31:25 +0500

    getting optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,14 @@
+
+const getCroppedImageUrl = (url:string) => {
+// big image thi ye jisy load kr rhy thy lekin ham usy cropr kr ky load krna chahty thy
+// ta ky slow net waly ko masla na ho. image ky url main ye dala wo crop ho gai. is website pe to kam kr gya ye
+// bad main ab croped pechy sy ai ha slwo wala bi load kr ly ga ab isy khud jitni dimenstion main dikhana ha wo dikha sakty han card main
+  const target='media/'
+  // length di ha ta ky media/ means last tak jaye / tak
+  const index= url.indexOf(target) + target.length;
+  //begineing sy ly ky akhir tak sary characters mil gaye us main crop add kr do
+   return url.slice(0,index) + 'crop/600/400/' + url.slice(index) // index sy ab last tak 
+   // we can verify by copying image url from network
+}
+
+export default getCroppedImageUrl;
