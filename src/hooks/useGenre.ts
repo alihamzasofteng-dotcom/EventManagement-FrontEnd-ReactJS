f843bbefe98d78e47cbce48b270173c72316427c
Hamza Ali <hamza.ali@ethizo.com>
2024-10-23 20:36:53 +0500

    ship genres with the app

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,11 +1,15 @@
 
 import useData from "./useData";
+import genres from "../data/genres";
 export interface Genre {
     // what we need from url  https://api.rawg.io/docs/#operation/games_list
     id: number;
     name: string;
-    image_background: string
+    image_background: string;
   }
 
-const useGenre = () => useData<Genre>('/genres');
+// genre ab baakcend ko nai call kr rhy balky object pkr ky data file main store kra lya ha
+// kue ky ye change ni hota to instantly nzar aye
+//const useGenre = () => useData<Genre>('/genres');
+const useGenre = () => ({data: genres, isLoading: false, error:null});
 export default useGenre;
