041d2f47498836ae0193210adcfc75b3f8f32f96
Hamza Ali <hamza.ali@ethizo.com>
2024-10-25 17:27:00 +0500

    implement infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,10 @@
-import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "./../App";
+import React from "react";
 
 // we added new prop get its vale and pass it to games hook
 interface Props {
@@ -13,29 +14,43 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //render six skeleton loaders.
   if (error) return <Text>{error.message}</Text>;
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding="10"
-      spacing={5}
-    >
-      {/* Display skeletons if loading */}
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
-          </GameCardContainer>
+    <Box padding="10">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={5}>
+        {/* Display skeletons if loading */}
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {/* Display game cards when data is available */}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {/* Display game cards when data is available */}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      </SimpleGrid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} marginY={5}>
+          {isFetchingNextPage ? "Loading..." : "Load More"}{" "}
+        </Button>
+      )}
+    </Box>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 import { GameQuery } from './../App';
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from './usePlatforms';
 
@@ -16,18 +16,20 @@ export interface Game {
     rating_top: number
   }
   //idr fetch is liye ky to tell the type of the data that useQuery expects from the queryFn
-  const useGames = (gameQuery:GameQuery) => useQuery<FetchResponse<Game>, Error>({
+  const useGames = (gameQuery:GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['gameQuery', gameQuery], // kuch bi is main change ho ga refetch ho ga just like dependency
-    queryFn: () => apiClient.getAll({
+    queryFn: ({pageParam=1, }) => apiClient.getAll({ 
       params: {
         genres: gameQuery.genre?.id , 
         parent_platforms : gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
+        search: gameQuery.searchText,
+        page: pageParam
        }
     }),
-    staleTime: 24*60*60*1000,
-   //initialData: {count: platforms.length, results: platforms}  
+    getNextPageParam: (lastPage, allPages) =>{
+      return lastPage.next ? allPages.length +1 : undefined;
+    }
   });
 
 
