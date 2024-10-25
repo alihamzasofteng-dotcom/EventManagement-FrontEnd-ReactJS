acf56742d9c03aa3212087b2bccfad98f80b1e47
Hamza Ali <hamza.ali@ethizo.com>
2024-10-25 16:59:48 +0500

    reuseable api client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,10 @@
 import { GameQuery } from './../App';
 import { useQuery } from "@tanstack/react-query";
-import apiClient,{ FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from './usePlatforms';
+
+const apiClient  = new APIClient<Game>('/games');
+
 
 //array ky object main kya kya ho ga
 export interface Game {
@@ -12,18 +15,17 @@ export interface Game {
     metacritic: number,
     rating_top: number
   }
-
+  //idr fetch is liye ky to tell the type of the data that useQuery expects from the queryFn
   const useGames = (gameQuery:GameQuery) => useQuery<FetchResponse<Game>, Error>({
     queryKey: ['gameQuery', gameQuery], // kuch bi is main change ho ga refetch ho ga just like dependency
-    queryFn: () => apiClient.get<FetchResponse<Game>>('/games',{
-      // passing query string paramters
-        params: {
+    queryFn: () => apiClient.getAll({
+      params: {
         genres: gameQuery.genre?.id , 
         parent_platforms : gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText
        }
-    }).then(res=> res.data),
+    }),
     staleTime: 24*60*60*1000,
    //initialData: {count: platforms.length, results: platforms}  
   });
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T>{
     count: number,
     results: T[]
   }   
 
-export default axios.create({
+const axiosInstance= axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
         key: '2eef00dc15484404bdf71e4a76d26a1a'
     }
 })
+
+class APIClient<T>{
+    endPoint: string;
+
+    constructor(endPoint: string){
+        this.endPoint= endPoint
+    }
+
+    getAll =  (config?: AxiosRequestConfig) => { // this parameter only need in game hook thats why optional 
+        return axiosInstance
+                .get<FetchResponse<T>>(this.endPoint, config) // in uor games hook we need a config object for passing query string to backend
+                .then(res => res.data);
+    }
+}
+
+export default APIClient;
