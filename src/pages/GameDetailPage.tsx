6329bbd2d7ac9b7faddf6ebbb37582b0dd320479
Hamza Ali <alihamza.softeng@gmail.com>
2024-10-29 23:23:02 +0500

    Build game Screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GamesAttributes from "../components/GamesAttributes";
+import GameScreenshots from "../components/GameScreenshots";
 import GameTrailor from "../components/GameTrailor";
 import useGame from "../hooks/useGame";
 
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GamesAttributes game={game} />
       <GameTrailor gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
